c92032819a10554b796133436e4d700ee6bff828
MarouaBoudabous <boudabous.maroua@gmail.com>
2014-05-27 05:41:57 -0400

    les classes entity

---
+++ src/main/java/insat/tn/artnet/utils/CassandraConnection.java
@@ -0,0 +1,26 @@
+package insat.tn.artnet.utils;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Host;
+import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.Session;
+
+public class CassandraConnection {
+	
+	private static Cluster cluster;
+	private static Session session;
+	
+	public static void connect(String node) {
+		   cluster = Cluster.builder().addContactPoint(node).build();
+		   Metadata metadata = cluster.getMetadata();
+		   System.out.printf("Connected to cluster: %s\n", 
+		         metadata.getClusterName());
+		   for ( Host host : metadata.getAllHosts() ) {
+		      System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n",
+		         host.getDatacenter(), host.getAddress(), host.getRack());
+		   }
+		   session= cluster.connect("artnetspace");
+
+		}
+
+}
